4aec3dbcfb323f9418175f594056054acabb66ee
Adrian Jobst <adrian.jobst@hotmail.de>
2023-12-07 21:30:35 +0100

    changed config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -929,7 +929,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 100, 100, 100, 10, 40}
+#define DEFAULT_MAX_FEEDRATE          { 100, 100, 100, 100, 100}
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -942,7 +942,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION       { 3000, 3000, 3000, 0.05, 500 }
+#define DEFAULT_MAX_ACCELERATION       { 3000, 3000, 3000, 1500, 1500 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
